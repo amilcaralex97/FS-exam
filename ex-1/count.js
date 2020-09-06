90d47f2ccb5a1960a342cfb91ab0a28443cb2c5b
Amílcar Sánchez <45496427+amilcaralex97@users.noreply.github.com>
2020-09-06 11:21:19 -0500

    count function

--- ex-1/count.js
+++ ex-1/count.js
@@ -1,10 +1,8 @@
-const count = () => {
-  let number = 0;
-  return () => {
-    return (number = number + 1);
+const count = (function () {
+  var counter = 0;
+  return function () {
+    return (counter += 1);
   };
-};
+})();
 
-console.log(count());
-console.log(count());
-console.log(count());
+module.exports = count;

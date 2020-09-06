e636170bd9e3dbf858da528c7ed3562797a318fa
Amílcar Sánchez <45496427+amilcaralex97@users.noreply.github.com>
2020-09-06 11:33:13 -0500

    var changed to let

--- ex-1/count.js
+++ ex-1/count.js
@@ -1,5 +1,9 @@
+//A closure gives you access to an outer function’s scope from an inner function.
+//It makes counter variable "private in coount function and"
+//the child function can access to it and modify it and can protect the counter variable
+
 const count = (function () {
-  var counter = 0;
+  let counter = 0;
   return function () {
     return (counter += 1);
   };
